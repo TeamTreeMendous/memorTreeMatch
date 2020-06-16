284fc70af48c2c11cc62d72d9c5812812c824603
Robert Duhig <robertduhig@zoho.com>
2020-06-16 02:30:16 -0400

    fixed bugs

--- script.js
+++ script.js
@@ -209,7 +209,7 @@ memorTreeMatch.buttons = function() {
 // * * * * API PROMISE
 memorTreeMatch.getPlant = function(id) {
     return $.ajax({
-        url: "http://proxy.hackeryou.com",
+        url: "https://proxy.hackeryou.com",
         method: "GET",
         dataType: "json",
         data: {
